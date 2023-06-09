9dbd2f07f7cfe4bf0ac721d1b8799321eda60478
yeeyeeeyee <aagg50211@gmail.com>
2023-06-10 01:42:03 +0800

    test

--- 股票/realtime.py
+++ 股票/realtime.py
@@ -1,6 +1,8 @@
 import twstock
 
-stock_data = twstock.realtime.get(["0050", "0052","0056","2330"])
+stock_data = twstock.realtime.get(["0050", "0052","0055","0056","2330","006204","006208",
+                                   "00637L","00646","00679B","00690","00692","00701",
+                                   "00708L",'00712','00713','00728','00730',"00731",])
 
 #print(stock_data["0050"]['realtime'])
 
@@ -10,6 +12,42 @@ for stock_code in stock_data:
     if "success" == stock_code:
         break
 
-    best_ask_volume = stock_data[stock_code]['info']
+    best_ask_volume = stock_data[stock_code]['info']["name"]
     print(f"{stock_code} 的 best_ask_volume 為: {best_ask_volume}")
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'00733',
+'00751B',
+'00752',
+'00757',
+'00770',
+'00773B',
+'00830',
+"00850"
+'00878'
+'00881'
+'00882'
+'00885'
+'00888'
+'00892'
+'00894'
+'00900'
+'00907'
+'00771'
