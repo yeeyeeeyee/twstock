a35ad46d502331995e02baecafe83f9f189f3734
yeeyeeeyee <aagg50211@gmail.com>
2023-07-15 21:18:13 +0800

    自動寬度

--- xlwings_test/merge.py
+++ xlwings_test/merge.py
@@ -18,31 +18,20 @@ data_list = df.iloc[:, 1].astype(str).tolist()
 # 获取A工作表和B工作表
 original_sheet = wb.sheets['a']
 
-# 创建字典以加快查找
-data_dict = {target: True for target in data_list}
 n=1
 for target in data_list:
     n+=1
     for i ,element in enumerate(my_list):
         if target in element:
             print(f"Element '{element}' contains target '{target}' at position {i}")
-            #print(wb.sheets[i].name)
+            # 将第4行的文字向下移动一格
             wb.sheets[i].range("4:4").api.Insert()
-            original_sheet.range(f'B{n}:N{n}').api.Copy(wb.sheets[i].range('B5').api)
+            #複製
+            original_sheet.range(f'A{n}:P{n}').api.Copy(wb.sheets[i].range('A5').api)
+            wb.sheets[i].autofit()
 # 保存并关闭工作簿
-wb.save() 
-
-""" 
-# 获取A工作表和B工作表
-original_sheet = wb.sheets['a']
-
-# 将第4行的文字向下移动一格
-sheet_b.range("4:4").api.Insert()
-# 复制B2到N2的范围到B工作表的第4行
-sheet_a.range('B2:N2').api.Copy(sheet_b.range('B5').api)
+#wb.save() 
 
 
-
- """
-#print(data_list)
+print(data_list)
 #print(my_list)
