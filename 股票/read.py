5877ca96c168a9b02ccdb6b8e2ae20be4e317da4
yeeyeeeyee <aagg50211@gmail.com>
2023-07-17 15:04:05 +0800

    1.開啟檔案變成副函式 2.修改讀取josn位置 3.添加重新讀取即時資料

--- 股票/read.py
+++ 股票/read.py
@@ -5,9 +5,30 @@ import time as t
 import requests
 import stock_end
 import json
+import xlwings as xw
+import classification
 
+def open_excel_file(file,sheet_name:str=""):
+    try:
+        workbook = xw.Book(file)
+    except:
+        app = xw.App(visible=True, add_book=False)
+        workbook = app.books.open(file)
+    if sheet_name == "":
+        sheet = workbook.sheets.active
+    else:
+        sheet = workbook.sheets[sheet_name]
+    return workbook, sheet
+
+def reload_data():
+    print("是否重新載入資料 y/n")
+    input_str = input("輸入:")
+    if input_str.lower()  == "y":
+        get_stock.update_realtime_data(data_list,sheet)
+        reload_data()
+#-------------------------------------------------
 # 讀取 JSON 檔案
-with open('股票\\seting.json', 'r',encoding='utf-8') as file:
+with open('股票\\setting.json', 'r',encoding='utf-8') as file:
     config = json.load(file)
 #讀取
 read_file = config['read_file']
@@ -19,6 +40,8 @@ write_sheet=config['write_sheet']
 
 # 關閉檔案
 file.close()
+
+#-------------------------------------------------------------------------
 # 读取Excel文件
 df = pd.read_excel(read_file,read_sheet)
 
@@ -33,14 +56,10 @@ now = datetime.now().time()
 # 設定下午1點半的時間
 closing_time = time(13, 40)
 
-workbook,sheet = get_stock.main(write_file,write_sheet)
+#開啟檔案
+workbook,sheet=open_excel_file(write_file,write_sheet)
 
-#測試用
 
-#詳細資料
-#stock_end.update_data(data_list,sheet)
-#即時資料
-#get_stock.update_realtime_data(data_list,sheet)
 
 
 error_count = 0  # 错误计数器
@@ -69,7 +88,27 @@ while now < closing_time:
 #最後補齊即時資料
 get_stock.update_realtime_data(data_list,sheet)
 
-print("結束")
+reload_data()
+
+print("即時資料-結束")
+
+print("資料分類-開始")
+workbook,sheet=open_excel_file(read_file,read_sheet)
+classification.classification(data_list,sheet)
+print("資料分類-結束")
+input("------請按任意鍵結束-------")
+#-------------------------------------------------------------------------
+ #測試用
+
+#詳細資料
+#stock_end.update_data(data_list,sheet)
+#即時資料
+#get_stock.update_realtime_data(data_list,sheet)
+#分類
+
+#workbook,sheet=open_excel_file(read_file,read_sheet)
+#classification.classification(data_list,sheet)
+
 
 
 
