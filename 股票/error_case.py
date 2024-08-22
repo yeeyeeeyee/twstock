e7a674475a3e71b6fe26eb72ab8102ab8118f89b
yeeyeeeyee <aagg50211@gmail.com>
2024-08-22 20:36:53 +0800

    fix error_case等待給用戶查看: 當發生錯誤時讓終端停下來讓用戶查看問題

--- 股票/error_case.py
+++ 股票/error_case.py
@@ -2,6 +2,10 @@ def error_case(error_message):
     print(error_message)
     print("------出現異常請截圖告知-------")
     print("------請按任意鍵結束-------")
+    input()
+
+if __name__ == '__main__':
+    error_case("test")
 
 
 
